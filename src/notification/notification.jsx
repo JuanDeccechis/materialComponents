00d59b39b8aa5260939674a4d82fb73272c01a32
jcdeccechis <jcdeccechis@technisys.com>
2021-07-11 12:16:31 -0300

    mejoras a notif y settings

--- src/notification/notification.jsx
+++ src/notification/notification.jsx
@@ -10,54 +10,58 @@ class Notification extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        value: 10,
+        value: 5,
         showNotifications: false,
+        notificationData: [{
+            "text": "user1"
+        }, {
+            "text": "user2"
+        }, {
+            "text": "Pepe te envió una solicitud"
+        }, {
+            "text": "María compartió una foto"
+        }, {
+            "text": "A Gerónimo le gustó tu comentario"
+        }],
     };
     this.handleClickNotification = this.handleClickNotification.bind(this);
+    this.handleClickDelete = this.handleClickDelete.bind(this);
   }
 
   handleClickNotification() {
       this.setState({ showNotifications: !this.state.showNotifications });
   }
 
+  handleClickDelete(index) {
+      console.log(index);
+  }
+
   render(){
-    const { value, showNotifications } = this.state;
+    const { value, showNotifications, notificationData } = this.state;
 
       return(
         <div className="notification-relative">
-        { value === 0 ?
-            <div className="notification-container">
-                <NotificationsIcon />
+            { value === 0 ?
+                <div className="notification-container">
+                    <NotificationsIcon />
+                </div>
+                :
+                <div className="notification-container notification-not-empty" onClick={this.handleClickNotification}>
+                <NotificationsActiveIcon />
+                <span className="notification-badge-value">{value}</span>
             </div>
-            :
-            <div className="notification-container notification-not-empty" onClick={this.handleClickNotification}>
-            <NotificationsActiveIcon />
-            <span className="notification-badge-value">{value}</span>
-        </div>
-        }
+            }
             { showNotifications &&
                 <div id="myDropdown" className="dropdown-content">
-                <div className="user_ctrl_box center">
-                    <div className="user_dropdown_menu" onClick={this.handleClickNotification}>
-                        
-                             <span>User1</span>      
-                    </div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickNotification}>
-                             <span>User2</span>      
-                    </div>
-                    <div className="dropdown-divider "></div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickNotification}>
-                    <span>Mi Cuenta</span>
-                    </div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickNotification}>
-                    <span>Centro de ayuda</span>
-                    </div>
-                    <div className="dropdown-divider"></div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickNotification}>
-                    <span>Cerrar sesion</span>
-                    </div>
-                </div>   
-            </div>
+                    <div className="user_ctrl_box center">
+                        { notificationData.map((notif, index) => 
+                            <div className="notification_user_dropdown_menu">
+                                <span onClick={this.handleClickNotification}>{notif.text}</span>
+                                <span className="notification-delete" onClick={(index) => this.handleClickDelete(index)}>X</span>
+                            </div>
+                        )}
+                    </div>   
+                </div>
             }
         </div>
       )
--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -3,6 +3,7 @@ import { withTheme } from '../context/ThemeContext';
 import Badge from '@material-ui/core/Badge';
 import SettingsIcon from '@material-ui/icons/Settings';
 import './settings.css';
+import { CONNREFUSED } from "dns";
 
 
 class Settings extends Component {
@@ -11,9 +12,15 @@ class Settings extends Component {
     this.state = {
         value: 10,
         showSettings: false,
+        showNotifications: true,
     };
     this.handleClickSettings = this.handleClickSettings.bind(this);
-  }
+    this.handleSwitch = this.handleSwitch.bind(this);
+}
+
+handleSwitch(event){
+    this.setState({ showNotifications: !this.state.showNotifications });
+}
 
   handleClickSettings() {
       this.setState({ showSettings: !this.state.showSettings });
@@ -32,22 +39,30 @@ class Settings extends Component {
             { showSettings &&
                 <div id="mySettings" className="dropdown-content">
                 <div className="user_ctrl_box center">
-                    <div className="user_dropdown_menu" onClick={this.handleClickSettings}>
-                        
-                             <span>User1</span>      
+                    <div className="settings_user_dropdown_menu">
+                    <span>
+                        <div className="container-switcher">
+                            <span className="label-switcher">Mostrar notificaciones: </span>
+                            <label class="switch">
+                                <input type="checkbox" checked={this.state.showNotifications} onChange={this.handleSwitch} />
+                                <span class="slider round"></span>
+                            </label>
+                        </div>
+
+                    </span>
                     </div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickSettings}>
+                    <div className="settings_user_dropdown_menu" onClick={this.handleClickSettings}>
                              <span>User2</span>      
                     </div>
                     <div className="dropdown-divider "></div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickSettings}>
+                    <div className="settings_user_dropdown_menu" onClick={this.handleClickSettings}>
                     <span>Mi Cuenta</span>
                     </div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickSettings}>
+                    <div className="settings_user_dropdown_menu" onClick={this.handleClickSettings}>
                     <span>Centro de ayuda</span>
                     </div>
                     <div className="dropdown-divider"></div>
-                    <div className="user_dropdown_menu" onClick={this.handleClickSettings}>
+                    <div className="settings_user_dropdown_menu" onClick={this.handleClickSettings}>
                     <span>Cerrar sesion</span>
                     </div>
                 </div>   
